862b8a089fa886498b4df0d28edd3f2dda12ed95
tgisme <taylorgarrett523@gmail.com>
2022-04-08 10:48:36 -0600

    Add project files.

---
+++ TestingUnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingUnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingUnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68c19a4a-2c40-44f5-b27e-01160b1df66f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestingUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestingUnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public IWebDriver driver;
+        public readonly string site;
+        public UnitTest1()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            site = "https://mango-flower-00e2aa010.azurestaticapps.net/";
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl(site);
+            System.Threading.Thread.Sleep(2000);
+            var blueBox = driver.FindElement(By.ClassName("color-blue"));
+            blueBox.Click();
+            System.Threading.Thread.Sleep(2000);
+            var container = driver.FindElement(By.ClassName("color-container"));
+
+            Assert.AreEqual("color-container color-blue", container.GetAttribute("class"));
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
